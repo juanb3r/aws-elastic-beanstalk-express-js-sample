720219494b7b0578381b735e04c03347fb5a2487
juanber <jmesteman@hotmail.com>
2020-11-27 10:18:58 -0500

    Redireccionamos

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Mira quién llegó, soy yo, el dueño del party'));
+app.get('/', (req, res) => res.send('/index.html'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
