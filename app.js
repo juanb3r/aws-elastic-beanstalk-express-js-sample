63d7c4366cfafc29c3808bc03554c5e17c073fef
juanber <jmesteman@hotmail.com>
2020-11-27 08:30:51 -0500

    Cambio de mensaje

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Mira quién llegó, soy yo, el dueño del party'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
